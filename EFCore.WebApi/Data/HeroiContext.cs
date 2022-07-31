4804bf7c8e506b8f2f748dbdf57178363243f68a
guilherme eduardo <guilherme09112001@hotmail.com>
2022-07-31 19:28:46 -0300

    segundoInicializando App

--- EFCore.WebApi/Data/HeroiContext.cs
+++ EFCore.WebApi/Data/HeroiContext.cs
@@ -1,6 +1,19 @@
-﻿namespace EFCore.WebApi.Data
+﻿using EFCore.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.WebApi.Data
 {
-    public class HeroiContext
+    public class HeroiContext : DbContext
     {
+        public DbSet<Heroi>? Herois { get; set; }
+        public DbSet<Batalha>? Batalhas { get; set; }
+        public DbSet<Arma>? Armas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Password=meuteste;Persist Security Info=True;User ID=diamante;Initial Catalog=herois;Data Source=GUILHERME\\SQLEXPRESS");
+        }
+
     }
 }
--- EFCore.WebApi/Models/Heroi.cs
+++ EFCore.WebApi/Models/Heroi.cs
@@ -2,5 +2,11 @@
 {
     public class Heroi
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public Batalha Batalha { get; set; }
+        public int BatalhaId { get; set; }
+
     }
 }
